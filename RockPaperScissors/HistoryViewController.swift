66d753df722513fd1fcfc26293c843548ea39791
Mairo <miroslav.mikove@gmail.com>
2022-03-30 01:16:06 +0200

    Initial Commit

---
+++ RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,70 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by mairo on 28/03/2022.
+//  Copyright © 2022 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Properties
+    
+    var history: [RPSMatch]!
+    
+    // MARK: Outlet
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Table View Delegate
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return history.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /*
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyRecordLine", for: indexPath)
+        
+        // cell?.textlabel?.text = history[indexPath.row]
+        let match = self.history[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel?.text = victoryStatusDescription(match)
+        cell.detailTextLabel?.text = "\(match.p1) vs. \(match.p2)"
+        
+        return cell
+        */
+        
+        let CellID = "HistoryCell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellID, for: indexPath)
+        
+        let match = self.history[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel!.text = victoryStatusDescription(match)
+        cell.detailTextLabel!.text = "\(match.p1) vs. \(match.p2)"
+        
+        return cell
+    }
+    
+    func victoryStatusDescription(_ match: RPSMatch) -> String {
+        
+        if (match.p1 == match.p2) {
+            return "Tie."
+        } else if (match.p1.defeats(match.p2)) {
+            return "Win!"
+        } else {
+            return "Loss."
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func dismissHistory(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
